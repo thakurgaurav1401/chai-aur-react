ef950822517e6ab97c67199c8b30357e23809cf4
thakurgaurav1401 <thakurgaurav1401@gmail'com>
2025-10-06 00:11:15 +0530

    component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -6,8 +6,12 @@ function App() {
   
 
   return (
-    
+    <> 
     <Chai/>
+    <h1>Gaurav you can complete this within one month</h1>
+    <p>Adi and Pradeep is good boy but some time they are not.</p>
+    </>
+   
   )
 }
 
